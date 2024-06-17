145d5c22c7c4dfd58751a431f9908ffb90e2d7d5
edwin <gtr0206edwin@gmail.com>
2024-06-17 21:37:58 +0800

    The frame can now show test data successfully.

--- app/src/main/java/lab/nanoelectronic/DataHandler.java
+++ app/src/main/java/lab/nanoelectronic/DataHandler.java
@@ -2,29 +2,122 @@ package lab.nanoelectronic;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class DataHandler {
     private int tableNum;
-    private Queue<float[][]> tables = new LinkedList<float[][]>();
-    private Queue<String[]> columnNames = new LinkedList<String[]>();
+    private Double[][][] tables = new Double[2][][];
+    private String[][] columnNames = new String[2][];
+    private String[] fileName = new String[2];
+    private boolean[] available = new boolean[2];
+    private boolean[] mostRecentUse = new boolean[2];
 
     DataHandler() {
-        ;
+        tableNum = 0;
+        available[0] = false;
+        available[1] = false;
+        mostRecentUse[0] = false;
+        mostRecentUse[1] = false;
     }
 
     public int getTableNum() {
-        return 0;
+        return tableNum;
     }
 
-    public double openTable(String path) {
-        return 0.0;
+    // for test
+    int openTable() {
+        System.err.println("openTable()");
+        available[0] = true;
+        mostRecentUse[0] = true;
+        mostRecentUse[1] = false;
+        tables[0] = new Double[2][];
+        tables[0][0] = new Double[4];
+        tables[0][1] = new Double[3];
+        tables[0][0][0] = 1.5;
+        tables[0][0][1] = 2.5;
+        tables[0][0][2] = 3.5;
+        tables[0][0][3] = 4.0;
+        tables[0][1][0] = 4.5;
+        tables[0][1][1] = 5.5;
+        tables[0][1][2] = 6.5;
+        columnNames[0] = new String[3];
+        columnNames[0][0] = "A";
+        columnNames[0][1] = "B";
+        columnNames[0][2] = "C";
+
+        tableNum = 1;
+
+        return 1;
     }
 
-    private void addTable() {
-        ;
+    int openTable(String path, String fileType) {
+        int status = 0;
+        // check if exist
+        if (path.equals(fileName[0]) || path.equals(fileName[1])) {
+            return status;
+        }
+
+        // choose a slot for new table
+        int tableIndex;
+        if (available[0] == false) {
+            tableIndex = 0;
+        } else if (available[1] == false) {
+            tableIndex = 1;
+        } else {
+            tableIndex = mostRecentUse[0] ? 1 : 0;
+        }
+
+        status = loadFile(tableIndex, path, fileType);
+        if (status != 1) {
+            return status;
+        }
+        available[tableIndex] = true;
+        mostRecentUse[tableIndex] = true;
+        mostRecentUse[1 - tableIndex] = false;
+        fileName[tableIndex] = path;
+
+        // load file
+        
+
+        return status;
+    }
+
+    int loadFile(int tableIndex, String path, String fileType) {
+        int status = 1;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+        } catch (IOException e) {
+            status = 2;
+            //e.printStackTrace();
+            return status;
+        }
+
+        return status;
     }
 
     private void removeTable() {
         ;
+    }
+
+    public Double[][] getTable() {
+        System.err.println("getTable()");
+        return tables[0];
+    }
+    public String[] getColumnName() {
+        System.err.println("getColumnName()");
+        return columnNames[0];
+    }
+
+    public Double[][] getTable(int index) {
+        return null;
+    }
+
+    public String[] getColumnName(int index) {
+        return null;
     }
 }
--- app/src/main/java/lab/nanoelectronic/Main.java
+++ app/src/main/java/lab/nanoelectronic/Main.java
@@ -4,22 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class MyJFrame extends JFrame {
-    private JPanel sideBar;
+class MainFrame extends JFrame {
+    private JPanel sideBar, mainNotScrollPane;
     private JScrollPane mainPane;
+    private JTextField fileJText;
+    private JButton fileConfirmJButton;
 
-    MyJFrame() {
-        init();
-        
-        //JButton btnC = new JButton("按鈕元件C");
-        //contentPane.add(btnC, BorderLayout.CENTER);
+    private JTable[] jTables = new JTable[2];
 
-        
-        
+
+
+    private DataHandler dataHandler = null;
+
+    MainFrame() {
+        initHandler();
+        initLayout();
+
         setVisible(true);
     }
 
-    void init() {
+    private void initLayout() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 1000, 700);
         setLayout(new BorderLayout(0, 0));
@@ -33,27 +37,85 @@ class MyJFrame extends JFrame {
         //mainPane = new JScrollPane();
         //add(mainPane, BorderLayout.CENTER);
     }
+    
+    private void initHandler() {
+        dataHandler = new DataHandler();
+    }
 
-    void sideBarLayout() {
+    private void sideBarLayout() {
         sideBar = new JPanel();
         sideBar.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
 
         JLabel fileJLabel = new JLabel("file");
         sideBar.add(fileJLabel);
 
-        JTextField fileJText = new JTextField();
-        fileJText.setColumns(20);
+        fileJText = new JTextField();
+        fileJText.setColumns(10);
         sideBar.add(fileJText);
+
+        fileConfirmJButton = new JButton("confirm");
+        sideBar.add(fileConfirmJButton);
+        fileConfirmJButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.err.println("pressed");
+                if (!fileJText.getText().isEmpty()) {
+                    handleData(fileJText.getText());
+                }
+            }
+        });
+
     }
 
-    void mainPanelLayout() {
+    private void mainPanelLayout() {
         mainPane = new JScrollPane();
-        mainPane.setLayout(null);
+    }
+
+    private void handleData(String file) {
+        System.err.println("handleData");
+
+        //int openStatus = dataHandler.openTable();    // test data
+        int openStatus = dataHandler.openTable(fileJText.getText(), getFileType());
+        if (openStatus == 0) {
+            // file Exist
+        } else if (openStatus == 1) {
+            // open success
+            showData();
+        } else if (openStatus == 2) {
+            // file doesn't exist
+            ;
+        }
+    }
+
+    String getFileType() {
+        return "LabVIEW";
+    }
+
+    private void showData() {
+        System.err.println("showData");
+        if (dataHandler == null) {
+            System.err.println("error: data handler not found.");
+            return;
+        }
+
+        remove(mainPane);
+
+        if (dataHandler.getTableNum() == 1) {
+            Double[][] table = dataHandler.getTable();
+            jTables[0] = new JTable(table, dataHandler.getColumnName());
+            jTables[0].setFillsViewportHeight(true);
+            System.err.println("add");
+        } else if (dataHandler.getTableNum() == 2) {
+            System.err.println("Error: file not found.");
+        }
+        mainPane = new JScrollPane(jTables[0]);
+        add(mainPane);
+        repaint();
+        revalidate();
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        MyJFrame frame = new MyJFrame();
+        MainFrame frame = new MainFrame();
     }
 }
